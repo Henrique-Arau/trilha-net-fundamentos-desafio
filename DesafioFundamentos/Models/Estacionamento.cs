9b6e88147cd6c2627358a88211ce5e2e613ff3d8
Henrique-Arau <penielh43@gmail.com>
2023-10-02 13:35:48 -0300

    Remover placa

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -23,11 +23,14 @@ namespace DesafioFundamentos.Models
 
         public void RemoverVeiculo()
         {
-            Console.WriteLine("Digite a placa do veículo para remover:");
+            
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
             string placa = "";
+            Console.WriteLine("Digite a placa do veículo para remover:");
+            placa = Console.ReadLine();
+
 
             // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
